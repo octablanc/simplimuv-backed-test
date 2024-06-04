62746dfdc5e73d2e24ba83d06b232cf7a8d627f3
octablanc <octaivio.blanc@phinxlab.com>
2024-06-04 10:22:44 -0300

    Add pagination on products endpoint

--- src/controllers/Product.controller.ts
+++ src/controllers/Product.controller.ts
@@ -7,13 +7,36 @@ import ProductVariantModel from "../models/ProductVariant.model";
 import VariantModel from "../models/Variant.model";
 import VariantDetailModel from "../models/VariantDetail.model";
 import VariantImageModel from "../models/VariantImage.model";
+import Joi from "joi";
 const productRouter = express.Router();
+
+const getProductsQuerySchema = Joi.object({
+    category_name: Joi.string().required(),
+    uuid: Joi.string().uuid()
+});
+
+const pageSchema = Joi.object({
+    page: Joi.number().integer().min(1)
+});
 
 async function getProducts(req: Request, res: Response) {
     const { category_name } = req.params;
-    const { uuid } = req.query;
+    const { uuid, page } = req.query;
+    const { error: errorCategoryOrId } = getProductsQuerySchema.validate({ category_name, uuid });
 
     try {
+        if (errorCategoryOrId) {
+            throw new Error(errorCategoryOrId.details[0].message);
+        }
+
+        if(page){
+            const { error: errorPage } = pageSchema.validate({page});
+
+            if (errorPage) {
+                throw new Error(errorPage.details[0].message);
+            }
+        }
+
         const query = {
             include: [
                 {
@@ -43,16 +66,15 @@ async function getProducts(req: Request, res: Response) {
                             ]
                         }
                     ],
-                    attributes: { exclude: [ "productId", "variantId"] }
+                    attributes: { exclude: ["productId", "variantId"] }
                 }
             ],
-            attributes: { exclude: [ "productBrandId", "productTypeId", "productCategoryId" ] }
+            attributes: { exclude: ["productBrandId", "productTypeId", "productCategoryId"] },
+            offset: page ? 100 * (Number(page) - 1) : 0,
+            limit: 100
         };
 
-        const result = uuid? await Product.findByPk(uuid.toString(), query) : await Product.findAll(query);
-
-        if (!result)
-            throw Error("Not found.");
+        const result = uuid ? await Product.findByPk(uuid.toString(), query) : await Product.findAll(query);
 
         return res.send(result);
     } catch (error) {
